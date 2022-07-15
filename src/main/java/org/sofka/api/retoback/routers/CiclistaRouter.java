1db25cfc07a6b353b5b75e76664224d848121ca5
duvan.botero <duvis07>
2022-07-15 12:44:26 -0500

    update

--- src/main/java/org/sofka/api/retoback/routers/CiclistaRouter.java
+++ src/main/java/org/sofka/api/retoback/routers/CiclistaRouter.java
@@ -2,18 +2,19 @@ package org.sofka.api.retoback.routers;
 
 import org.sofka.api.retoback.models.CiclistaDTO;
 import org.sofka.api.retoback.usecase.ciclistaUseCase.AgregarCiclistaUseCase;
+import org.sofka.api.retoback.usecase.ciclistaUseCase.ObtenerCiclistaUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 
@@ -29,6 +30,15 @@ public class CiclistaRouter {
         return route(
                 POST("/crear").and(accept(MediaType.APPLICATION_JSON)),
                 request -> request.bodyToMono(CiclistaDTO.class).flatMap(executor)
+        );
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> obtenerCiclistas(ObtenerCiclistaUseCase useCase) {
+        return route(GET("/obtenerCiclista").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(useCase.get(), CiclistaDTO.class))
         );
     }
 
--- src/main/java/org/sofka/api/retoback/usecase/ciclistaUseCase/ObtenerCiclistaUseCase.java
+++ src/main/java/org/sofka/api/retoback/usecase/ciclistaUseCase/ObtenerCiclistaUseCase.java
@@ -1,4 +1,33 @@
 package org.sofka.api.retoback.usecase.ciclistaUseCase;
 
-public class ObtenerCiclistaUseCase {
+import org.sofka.api.retoback.mappers.MapperCiclista;
+import org.sofka.api.retoback.models.CiclistaDTO;
+import org.sofka.api.retoback.repository.CiclistaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Flux;
+
+import java.util.function.Supplier;
+
+@Service
+@Validated
+public class ObtenerCiclistaUseCase implements Supplier<Flux<CiclistaDTO>> {
+
+
+    private final CiclistaRepository ciclistaRepository;
+    private final MapperCiclista mapperCiclista;
+
+    public ObtenerCiclistaUseCase(CiclistaRepository ciclistaRepository, MapperCiclista mapperCiclista) {
+        this.ciclistaRepository = ciclistaRepository;
+        this.mapperCiclista = mapperCiclista;
+    }
+
+
+    @Override
+    public Flux<CiclistaDTO> get() {
+        return ciclistaRepository.findAll()
+                .map(mapperCiclista.ciclistaAciclistaDto());
+
+
+    }
 }
